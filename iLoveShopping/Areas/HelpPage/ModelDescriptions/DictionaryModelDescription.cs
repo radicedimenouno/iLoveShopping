750fccd0860c0f31efcb912e22b4ae9a64fbc18c
Valentina Santangelo <radicedimenouno@gmail.com>
2016-02-21 23:39:18 +0100

    First commit

---
+++ iLoveShopping/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace iLoveShopping.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ iLoveShopping/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace iLoveShopping.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
